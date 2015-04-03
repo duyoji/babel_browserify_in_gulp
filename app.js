d7b36dcad8eff1ea6775c943f50c8cb3e948466e
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2015-04-03 19:01:37 +0900

    let, constの挙動確認用コード追加

--- app.js
+++ app.js
@@ -23,6 +23,38 @@ class Main {
         console.log('-----------------------');
         console.log( baseView.introduce() );
         console.log( baseView.toString() );
+
+        let main = new Main();
+        main.letTest();
+        main.constTest();
+    }
+
+    letTest() {
+        let a = 1;
+        {
+            let a = 2;
+            console.log('let in-block a : ' + a);
+            {
+                let a = 3;
+                console.log('let in-block-in-block a : ' + a);
+
+                a = 4;
+                console.log('let in-block-in-block a chenge : ' + a);
+            }
+        }
+
+        console.log('let out-block a : ' + a);
+    }
+
+    constTest() {
+        const a = 1;
+        {
+            // constで定義したものを上書きしようとするとコンパイルエラーが起きる
+            // a = 2;
+            // console.log('const in-block a : ' + a);
+        }
+
+        console.log('const out-block a : ' + a);
     }
 }
 
